f90b3085759f29b21e07f2eda6d8a77d87bab20a
Souriya Trinh <souriya.trinh@inria.fr>
2018-02-09 18:37:53 +0100

    Quick check to verify if covariance computation with generic edge, KLT and edge+KLT is coherent with old trackers.

--- tutorial/tracking/model-based/edges/tutorial-mb-edge-tracker.cpp
+++ tutorial/tracking/model-based/edges/tutorial-mb-edge-tracker.cpp
@@ -1,12 +1,11 @@
-//! \example tutorial-mb-edge-tracker.cpp
 #include <visp3/core/vpIoTools.h>
 #include <visp3/gui/vpDisplayGDI.h>
 #include <visp3/gui/vpDisplayOpenCV.h>
 #include <visp3/gui/vpDisplayX.h>
+#include <visp3/gui/vpPlot.h>
 #include <visp3/io/vpImageIo.h>
-//! [Include]
-#include <visp3/mbt/vpMbEdgeTracker.h>
-//! [Include]
+#include <visp3/mbt/vpMbGenericTracker.h>
+#include <visp3/mbt/vpMbEdgeKltTracker.h>
 #include <visp3/io/vpVideoReader.h>
 
 int main(int argc, char **argv)
@@ -14,6 +13,7 @@ int main(int argc, char **argv)
 #if defined(VISP_HAVE_OPENCV) && (VISP_HAVE_OPENCV_VERSION >= 0x020100)
   try {
     std::string videoname = "teabox.mpg";
+    int tracker_type = 0;
 
     for (int i = 0; i < argc; i++) {
       if (std::string(argv[i]) == "--name")
@@ -21,6 +21,8 @@ int main(int argc, char **argv)
       else if (std::string(argv[i]) == "--help") {
         std::cout << "\nUsage: " << argv[0] << " [--name <video name>] [--help]\n" << std::endl;
         return 0;
+      } else if (std::string(argv[i]) == "--tracker_type" && i+1 < argc) {
+        tracker_type = atoi(argv[i+1]);
       }
     }
     std::string parentname = vpIoTools::getParent(videoname);
@@ -36,125 +38,128 @@ int main(int argc, char **argv)
 #endif
               << "cao or wrl]" << std::endl;
     std::cout << "Tracker optional config files: " << objectname << ".[ppm]" << std::endl;
+    std::cout << "tracker_type: " << tracker_type << std::endl;
 
-    //! [Image]
-    vpImage<unsigned char> I;
-    vpCameraParameters cam;
-    //! [Image]
-    //! [cMo]
-    vpHomogeneousMatrix cMo;
-    //! [cMo]
+    vpImage<unsigned char> I, I2;
 
     vpVideoReader g;
     g.setFileName(videoname);
     g.open(I);
+    I2 = I;
 
-#if defined(VISP_HAVE_X11)
-    vpDisplayX display;
-#elif defined(VISP_HAVE_GDI)
-    vpDisplayGDI display;
-#elif defined(VISP_HAVE_OPENCV)
-    vpDisplayOpenCV display;
-#else
-    std::cout << "No image viewer is available..." << std::endl;
-    return 0;
-#endif
-
+    vpDisplayX display, display2;
     display.init(I, 100, 100, "Model-based edge tracker");
+    display2.init(I2, 700, 100, "Generic");
 
-    //! [Constructor]
-    vpMbEdgeTracker tracker;
-    //! [Constructor]
-    bool usexml = false;
-//! [Load xml]
-#ifdef VISP_HAVE_XML2
-    if (vpIoTools::checkFilename(objectname + ".xml")) {
-      tracker.loadConfigFile(objectname + ".xml");
-      usexml = true;
+    vpMbTracker *tracker;
+    std::vector<int> tracker_types = {vpMbGenericTracker::EDGE_TRACKER};
+    if (tracker_type == 0) {
+      tracker = new vpMbEdgeTracker;
+    } else if (tracker_type == 1) {
+      tracker = new vpMbKltTracker;
+      tracker_types[0] = vpMbGenericTracker::KLT_TRACKER;
+    } else {
+      tracker = new vpMbEdgeKltTracker;
+      tracker_types[0] = vpMbGenericTracker::EDGE_TRACKER | vpMbGenericTracker::KLT_TRACKER;
     }
-#endif
-    //! [Load xml]
-    if (!usexml) {
-      //! [Set parameters]
-      vpMe me;
-      me.setMaskSize(5);
-      me.setMaskNumber(180);
-      me.setRange(8);
-      me.setThreshold(10000);
-      me.setMu1(0.5);
-      me.setMu2(0.5);
-      me.setSampleStep(4);
-      tracker.setMovingEdge(me);
-      cam.initPersProjWithoutDistortion(839, 839, 325, 243);
-      tracker.setCameraParameters(cam);
-      //! [Set angles]
-      tracker.setAngleAppear(vpMath::rad(70));
-      tracker.setAngleDisappear(vpMath::rad(80));
-      //! [Set angles]
-      //! [Set clipping distance]
-      tracker.setNearClippingDistance(0.1);
-      tracker.setFarClippingDistance(100.0);
-      //! [Set clipping distance]
-      //! [Set clipping fov]
-      tracker.setClipping(tracker.getClipping() | vpMbtPolygon::FOV_CLIPPING);
-      //! [Set clipping fov]
-      //! [Set parameters]
-    }
-    //! [Set ogre]
-    tracker.setOgreVisibilityTest(false);
-    tracker.setOgreShowConfigDialog(false);
-    //! [Set ogre]
-    //! [Load cao]
-    if (vpIoTools::checkFilename(objectname + ".cao"))
-      tracker.loadModel(objectname + ".cao");
-    //! [Load cao]
-    //! [Load wrl]
-    else if (vpIoTools::checkFilename(objectname + ".wrl"))
-      tracker.loadModel(objectname + ".wrl");
-    //! [Load wrl]
-    //! [Set display]
-    tracker.setDisplayFeatures(true);
-    //! [Set display]
-    //! [Init]
-    tracker.initClick(I, objectname + ".init", true);
-    //! [Init]
+    vpMbGenericTracker tracker_generic(tracker_types);
 
+    tracker->loadConfigFile(objectname + ".xml");
+    tracker->loadModel(objectname + ".cao");
+    tracker->setDisplayFeatures(true);
+    tracker->setCovarianceComputation(true);
+    tracker->initClick(I, objectname + ".init", true);
+
+    tracker_generic.loadConfigFile(objectname + ".xml");
+    tracker_generic.loadModel(objectname + ".cao");
+    tracker_generic.setDisplayFeatures(true);
+    tracker_generic.setCovarianceComputation(true);
+    tracker_generic.initClick(I2, objectname + ".init", true);
+
+    vpPlot plot1(2, 700, 700, 0, 0, "MBT");
+    plot1.initGraph(0, 3);
+    plot1.initGraph(1, 3);
+
+    vpPlot plot2(2, 700, 700, 700, 0, "Generic");
+    plot2.initGraph(0, 3);
+    plot2.initGraph(1, 3);
+
+    int iter = 0;
+    std::vector<double> errors_cov_trans, errors_cov_rot;
     while (!g.end()) {
       g.acquire(I);
+      I2 = I;
+
       vpDisplay::display(I);
-      //! [Track]
-      tracker.track(I);
-      //! [Track]
-      //! [Get pose]
-      tracker.getPose(cMo);
-      //! [Get pose]
-      //! [Display]
-      tracker.getCameraParameters(cam);
-      tracker.display(I, cMo, cam, vpColor::red, 2);
-      //! [Display]
-      vpDisplay::displayFrame(I, cMo, cam, 0.025, vpColor::none, 3);
-      vpDisplay::displayText(I, 10, 10, "A click to exit...", vpColor::red);
+      vpDisplay::display(I2);
+
+      vpColVector cov_translation1(3), cov_rotation1(3);
+      vpColVector cov_translation2(3), cov_rotation2(3);
+      {
+        tracker->track(I);
+
+        vpHomogeneousMatrix cMo = tracker->getPose();
+        vpCameraParameters cam;
+        tracker->getCameraParameters(cam);
+        tracker->display(I, cMo, cam, vpColor::red, 2);
+
+        vpDisplay::displayFrame(I, cMo, cam, 0.025, vpColor::none, 3);
+        vpDisplay::displayText(I, 10, 10, "A click to exit...", vpColor::red);
+
+        vpMatrix cov = tracker->getCovarianceMatrix();
+        for (int i = 0; i < 3; i++) {
+          cov_translation1[i] = cov[i][i];
+          cov_rotation1[i] = cov[i+3][i+3];
+        }
+
+        plot1.plot(0, iter, cov_translation1);
+        plot1.plot(1, iter, cov_rotation1);
+      }
+
+      {
+        tracker_generic.track(I2);
+
+        vpHomogeneousMatrix cMo = tracker_generic.getPose();
+        vpCameraParameters cam;
+        tracker_generic.getCameraParameters(cam);
+        tracker_generic.display(I2, cMo, cam, vpColor::red, 2);
+
+        vpDisplay::displayFrame(I2, cMo, cam, 0.025, vpColor::none, 3);
+        vpDisplay::displayText(I2, 10, 10, "A click to exit...", vpColor::red);
+
+        vpMatrix cov = tracker_generic.getCovarianceMatrix();
+        for (int i = 0; i < 3; i++) {
+          cov_translation2[i] = cov[i][i];
+          cov_rotation2[i] = cov[i+3][i+3];
+        }
+
+        plot2.plot(0, iter, cov_translation2);
+        plot2.plot(1, iter, cov_rotation2);
+      }
+
+      for (int i = 0; i < 3; i++) {
+        errors_cov_trans.push_back( cov_translation1[i] - cov_translation2[i] );
+        errors_cov_rot.push_back( cov_rotation1[i] - cov_rotation2[i] );
+      }
+
       vpDisplay::flush(I);
-      if (vpDisplay::getClick(I, false))
+      vpDisplay::flush(I2);
+
+      if (vpDisplay::getClick(I, false) || vpDisplay::getClick(I2, false))
         break;
+
+      iter++;
     }
+
+    std::cout << "Mean error translation: " << vpMath::getMean(errors_cov_trans) << " ; Median: " << vpMath::getMedian(errors_cov_trans) << std::endl;
+    std::cout << "Mean error rotation: " << vpMath::getMean(errors_cov_rot) << " ; Median: " << vpMath::getMedian(errors_cov_rot) << std::endl;
+
     vpDisplay::getClick(I);
-//! [Cleanup]
-#ifdef VISP_HAVE_XML2
-    vpXmlParser::cleanup();
-#endif
-#if defined(VISP_HAVE_COIN3D) && (COIN_MAJOR_VERSION == 3)
-    SoDB::finish();
-#endif
-    //! [Cleanup]
+
+    delete tracker;
   } catch (vpException &e) {
     std::cout << "Catch a ViSP exception: " << e << std::endl;
   }
-#ifdef VISP_HAVE_OGRE
-  catch (Ogre::Exception &e) {
-    std::cout << "Catch an Ogre exception: " << e.getDescription() << std::endl;
-  }
-#endif
 #else
   (void)argc;
   (void)argv;
