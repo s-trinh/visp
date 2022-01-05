1a29ed5b176be40676f550866db5691c6ead92b2
Souriya Trinh <souriya.trinh+github@gmail.com>
2022-01-05 18:47:17 +0100

    Debug.

--- modules/vision/test/key-point/testKeyPoint-7.cpp
+++ modules/vision/test/key-point/testKeyPoint-7.cpp
@@ -2,27 +2,49 @@
 #include <string>
 
 #include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/features2d.hpp>
 
 int main()
 {
   std::cout << cv::getBuildInformation() << std::endl;
 
-  cv::Mat img = cv::Mat::ones(480, 640, CV_8U) * 255;
-  cv::rectangle(img, cv::Point(50,50), cv::Point(200, 200), cv::Scalar(127), -1);
-
-  cv::Ptr<cv::FeatureDetector> detector = cv::SiftFeatureDetector::create();
+  cv::Mat mask = cv::Mat::ones(img.rows, img.cols, CV_8U) * 255;
   {
-    std::vector<cv::KeyPoint> kp;
-    detector->detect(img, kp);
-    std::cout << "1) Nb keypoints: " << kp.size() << std::endl;
+    std::cout << "\nColor image:" << std::endl;
+    cv::Mat img = cv::imread("/Users/runner/ViSP-images/Klimt/Klimt.ppm", cv::IMREAD_COLOR);
+
+    cv::Ptr<cv::FeatureDetector> detector = cv::SiftFeatureDetector::create();
+    {
+      std::vector<cv::KeyPoint> kp;
+      detector->detect(img, kp);
+      std::cout << "1) Nb keypoints: " << kp.size() << std::endl;
+    }
+
+    {
+      std::vector<cv::KeyPoint> kp;
+      detector->detect(img, kp, mask);
+      std::cout << "2) Nb keypoints: " << kp.size() << std::endl;
+    }
   }
 
   {
-    std::vector<cv::KeyPoint> kp;
-    cv::Mat mask = cv::Mat::ones(img.rows, img.cols, CV_8U) * 255;
-    detector->detect(img, kp, mask);
-    std::cout << "2) Nb keypoints: " << kp.size() << std::endl;
+    std::cout << "\nGrayscale image:" << std::endl;
+    cv::Mat img = cv::imread("/Users/runner/ViSP-images/Klimt/Klimt.pgm", cv::IMREAD_GRAYSCALE);
+
+    cv::Ptr<cv::FeatureDetector> detector = cv::SiftFeatureDetector::create();
+    {
+      std::vector<cv::KeyPoint> kp;
+      detector->detect(img, kp);
+      std::cout << "1) Nb keypoints: " << kp.size() << std::endl;
+    }
+
+    {
+      std::vector<cv::KeyPoint> kp;
+      detector->detect(img, kp, mask);
+      std::cout << "2) Nb keypoints: " << kp.size() << std::endl;
+    }
   }
 
   return 0;
